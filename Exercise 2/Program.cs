5c0588e8bf202b2a85242b4215c2efb6536afd08
Eduard <dirol2016@list.ru>
2022-05-01 18:18:53 +0500

    2

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] StrToArray(string inptStr = "")
+{
+    int _countNumbers = 1;
+
+    for(int i = 0; i < inptStr.Length; i++){
+        if(inptStr[i] == ','){
+            _countNumbers++;
+        }
+    }
+
+    int[] numersArr = new int[_countNumbers];
+    int _index = 0;
+
+    for(int i = 0; i < inptStr.Length; i++){
+        string tempString = "";
+        
+        while(inptStr[i] != ','){
+            if(i != inptStr.Length - 1){
+                tempString += inptStr[i].ToString();
+                i++;
+            }
+            else{
+                tempString += inptStr[i].ToString();
+                break;
+            }
+        }
+        numersArr[_index] = Convert.ToInt32(tempString);
+        _index++;
+    }
+    return numersArr;
+}
+
+Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(b1 - b2)/(k1 - k2);
+double y = k2 * x + b2;
+
+Console.WriteLine("x = " + x + "; y = " + y);
